041cfdfe7e25d1353944007eb97ba64330eb7db1
David He <david.he@fortmatic.com>
2020-12-11 12:27:02 -0800

    hot reload contact card states

---
+++ packages/provider/src/core/css-styles.ts
@@ -0,0 +1,101 @@
+export const bottomRightStyle: Partial<CSSStyleDeclaration> = {
+  bottom: '15px',
+  right: '15px',
+};
+export const bottomLeftStyle: Partial<CSSStyleDeclaration> = {
+  bottom: '15px',
+  left: '15px',
+};
+export const topLeftStyle: Partial<CSSStyleDeclaration> = {
+  top: '15px',
+  left: '15px',
+};
+export const topRightStyle: Partial<CSSStyleDeclaration> = {
+  top: '15px',
+  right: '15px',
+};
+
+export const magicUiOverlayStyle: Partial<CSSStyleDeclaration> = {
+  display: 'block',
+  position: 'fixed',
+  width: '70px',
+  height: '70px',
+  borderRadius: '0',
+  border: 'none',
+  zIndex: '2147483647',
+  fontFamily: 'arial',
+};
+
+export const magicInputStyle: Partial<CSSStyleDeclaration> = {
+  height: '50px',
+  width: '88%',
+  borderRadius: '8px',
+  border: '1px solid #6851FF',
+  outline: 'none',
+  fontSize: '17px',
+  padding: '0 15px',
+  fontFamily: 'arial',
+};
+
+export const magicCloseBtnStyle: Partial<CSSStyleDeclaration> = {
+  height: '15px',
+  width: '15px',
+  cursor: 'pointer',
+  alignSelf: 'flex-end',
+};
+export const magicLoginBtnStyle: Partial<CSSStyleDeclaration> = {
+  height: '50px',
+  background: '#6851FF',
+  width: '100%',
+  borderRadius: '8px',
+  cursor: 'pointer',
+  color: 'white',
+  fontSize: '18px',
+  lineHeight: '50px',
+  textAlign: 'center',
+  marginBottom: '55px',
+};
+
+export const magicLogoutBtnStyle: Partial<CSSStyleDeclaration> = {
+  height: '45px',
+  background: '#FF5635',
+  width: '100%',
+  borderRadius: '8px',
+  cursor: 'pointer',
+  color: 'white',
+  fontSize: '18px',
+  lineHeight: '45px',
+  textAlign: 'center',
+  marginBottom: '45px',
+};
+
+export const magicLogoImgStyle: Partial<CSSStyleDeclaration> = {
+  width: '70px',
+  cursor: 'pointer',
+};
+
+export const magicBannerLogoImgStyle: Partial<CSSStyleDeclaration> = {
+  width: '60px',
+  marginBottom: '10px',
+};
+
+export const displayNoneStyle: Partial<CSSStyleDeclaration> = { display: 'none' };
+export const displayBlockStyle: Partial<CSSStyleDeclaration> = { display: 'block' };
+export const displayFlexStyle: Partial<CSSStyleDeclaration> = { display: 'flex' };
+
+export const contactCardStyle: Partial<CSSStyleDeclaration> = {
+  display: 'none',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  background: 'white',
+  position: 'absolute',
+  width: '250px',
+  height: '300px',
+  border: '1px solid #757575  ',
+  boxShadow: '0 12px 20px 0 rgba(0,0,0,.15)',
+  borderRadius: '12px',
+  padding: '20px',
+};
+
+export const magicAccountInfoStyle: Partial<CSSStyleDeclaration> = { display: 'flex' };
